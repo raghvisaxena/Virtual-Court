046e02471d71d7f0fdfd217d34482337f90d82e1
201951197@iiitvadodara.ac.in <201951197@iiitvadodara.ac.in>
2021-04-13 01:44:02 +0530

    static root updated

--- ecourt/settings.py
+++ ecourt/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "ecourt",
         "USER": "postgres",
-    "PASSWORD": "Manu#123@",
+    "PASSWORD": "ji14al9259",
         "HOST": "localhost",
         "PORT": "5432",
     }
@@ -128,6 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 import os
 
