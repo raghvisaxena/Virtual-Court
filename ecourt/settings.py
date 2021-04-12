486bc480cdfe456cd94316949b8c3f3bcfb1d39c
201951197@iiitvadodara.ac.in <201951197@iiitvadodara.ac.in>
2021-04-13 03:23:38 +0530

    envvar settings updated

--- ecourt/settings.py
+++ ecourt/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['e-court.herokuapp.com']
 
